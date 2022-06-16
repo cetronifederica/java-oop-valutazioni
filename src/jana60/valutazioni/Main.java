6a27d493aa422a86094d0e0000610921fb162d73
federicacetroni <cetronifederica97@gmail.com>
2022-06-16 17:15:00 +0200

    aggiunta classe main, metodo main, richiesta all'utente e stampa a tabella

--- src/jana60/valutazioni/Main.java
+++ src/jana60/valutazioni/Main.java
@@ -1,10 +1,56 @@
 package jana60.valutazioni;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * in cui create un array di studenti (i dati degli studenti possono essere
+		 * chiesti all’utente oppure inseriti nel codice con dei valori di test). L’id
+		 * di ogni studente deve essere univoco nell’array (possiamo usare un numero
+		 * progressivo). Il programma deve poi stampare a video per ogni studente se è
+		 * stato promosso o bocciato e quanti studenti sono stati promossi in tutto
+		 */
 
+		Scanner scan = new Scanner(System.in);
+
+		Studente[] studenti = new Studente[5];
+
+		int studPromossi = 0;
+
+		System.out.println("Inserisci i dati degli studenti dell tua classe: ");
+		for (int i = 0; i < studenti.length; i++) {
+
+			System.out.println("Studente " + (i + 1));
+			System.out.println("ID: ");
+			int id = Integer.parseInt(scan.nextLine());
+			System.out.println("Assenze: ");
+			int assenze = Integer.parseInt(scan.nextLine());
+			System.out.println("Media voti: ");
+			double mediaVoti = Double.parseDouble(scan.nextLine());
+
+			studenti[i] = new Studente(id, assenze, mediaVoti);
+
+			if (studenti[i].valutazione()) {
+				System.out.println("Promosso");
+				studPromossi++;
+			} else {
+				System.out.println("Bocciato");
+			}
+		}
+		String format = "%15s %15s %15s %15s";
+		System.out.println("Risultati studenti: ");
+		String intestazione = String.format(format, "ID", "Assenze", "Media voti", " Promosso/Bocciato");
+		System.out.println(intestazione);
+
+		for (int i = 0; i < studenti.length; i++) {
+
+			String infoStudente = studenti[i].infoStudenteFormat(format);
+			System.out.println(infoStudente);
+		}
+
+		System.out.println("Sono stati promossi in tutto " + studPromossi + " studenti");
+		scan.close();
 	}
-
 }
--- src/jana60/valutazioni/Studente.java
+++ src/jana60/valutazioni/Studente.java
@@ -5,11 +5,11 @@ public class Studente {
 	// attributi
 	int id;
 	int assenze;
-	float mediaVoti;
+	double mediaVoti;
 
 	// costruttori
 
-	Studente(int id, int assenze, float mediaVoti) {
+	Studente(int id, int assenze, double mediaVoti) {
 		this.id = id;
 		this.assenze = assenze;
 		this.mediaVoti = mediaVoti;
@@ -22,13 +22,25 @@ public class Studente {
 
 		if (assenze < 25 && mediaVoti >= 2) {
 			promosso = true;
-		} else if (assenze >= 25 && assenze <= 50 && mediaVoti > 2) {
+
+		} else if (assenze <= 50 && mediaVoti > 2) {
 			promosso = true;
-		} else {
-			promosso = false;
+
 		}
 		return promosso;
 
+	}
+
+	String infoStudente() {
+		String infoStudente = "ID: " + id;
+		infoStudente += "\nAssenze: " + assenze;
+		infoStudente += "\nMedia Voti: " + mediaVoti;
+		infoStudente += "\nPromosso o Bocciato: " + valutazione();
+		return infoStudente;
+	}
+
+	String infoStudenteFormat(String format) {
+		return String.format(format, id, assenze, mediaVoti, valutazione());
 	}
 
 }
